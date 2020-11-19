14302a84b52137d85ca3a2e8937e1efed4f04f6b
Neil Coutinho <njcoutinho@gmail.com>
2020-11-19 16:14:22 +0530

    feature: Update from and to currency dropdown options

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-console.log('hello world');
+const currencies = {
+    USD: 'United States Dollar',
+    AUD: 'Australian Dollar',
+    BGN: 'Bulgarian Lev',
+    BRL: 'Brazilian Real',
+    CAD: 'Canadian Dollar',
+    CHF: 'Swiss Franc',
+    CNY: 'Chinese Yuan',
+    CZK: 'Czech Republic Koruna',
+    DKK: 'Danish Krone',
+    GBP: 'British Pound Sterling',
+    HKD: 'Hong Kong Dollar',
+    HRK: 'Croatian Kuna',
+    HUF: 'Hungarian Forint',
+    IDR: 'Indonesian Rupiah',
+    ILS: 'Israeli New Sheqel',
+    INR: 'Indian Rupee',
+    JPY: 'Japanese Yen',
+    KRW: 'South Korean Won',
+    MXN: 'Mexican Peso',
+    MYR: 'Malaysian Ringgit',
+    NOK: 'Norwegian Krone',
+    NZD: 'New Zealand Dollar',
+    PHP: 'Philippine Peso',
+    PLN: 'Polish Zloty',
+    RON: 'Romanian Leu',
+    RUB: 'Russian Ruble',
+    SEK: 'Swedish Krona',
+    SGD: 'Singapore Dollar',
+    THB: 'Thai Baht',
+    TRY: 'Turkish Lira',
+    ZAR: 'South African Rand',
+    EUR: 'Euro',
+  };
+
+const from_currency = document.querySelector('[name="from_currency"]');
+const to_currency = document.querySelector('[name="to_currency"]');
+
+function generateOptions(currencies) {
+    return Object.entries(currencies)
+            .map(([currencyValue, currencyName]) => `<option value="${currencyValue}">${currencyValue} - ${currencyName}</option>`)
+            .join('');
+}
+
+let options = generateOptions(currencies);
+
+from_currency.innerHTML = options;
+to_currency.innerHTML = options;
+
+//console.log({from_currency, to_currency, options})
